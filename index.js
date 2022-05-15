de83a437f4f08b18edf64961df6073cc00cdd8dc
SaimaMeem <saimasultana1320@gmail.com>
2022-05-16 00:53:21 +0600

    my 3rd commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
