a11fa53b000edf147e8be9cc9521fbabd6cf31ec
SaimaMeem <saimasultana1320@gmail.com>
2022-05-16 00:51:46 +0600

    my 2nd commit

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ulkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
-    try{
-        await client.connect();
-        const serviceCollection = client.db('doctors_portal').collection('services');
-        app.get('/service',async (req,res)=>{
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-        })
-        
-    }
-    finally{
+  try {
+    await client.connect();
+    const serviceCollection = client.db('doctors_portal').collection('services');
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
 
-    }
+  }
+  finally {
+
+  }
 }
 run().catch(console.dir);
 app.get('/', (req, res) => {
@@ -32,5 +32,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Doctor's portal listening on port ${port}`)
+  console.log(`Doctor's portal listening on port`, port);
 })
